351ad072e4f893c3f461746976e8280043cdcadc
slaytrunks <slaytrunks@gmail.com>
2026-01-03 20:04:15 +0545

    [any message]

--- app/main.js
+++ app/main.js
@@ -1,11 +1,14 @@
+// //https://nodejs.org/api/readline.html
 const readline = require("readline");
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-
 // TODO: Uncomment the code below to pass the first stage
-rl.question("$ ", (answer) => {
+rl.question("$ ", (command) => {
+    console.log(`${command}: command not found`)
   rl.close();
 });
+
+

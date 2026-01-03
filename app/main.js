059e9a8fcdcb8c771eac6998f248cff07e0276a1
slaytrunks <slaytrunks@gmail.com>
2026-01-03 20:58:21 +0545

    implemented echo

--- app/main.js
+++ app/main.js
@@ -9,8 +9,14 @@ const rl = readline.createInterface({
 rl.setPrompt("$ ");
 rl.prompt();
 
-rl.on("line", async (c) => { // This event fires every time the user presses Enter after typing something.
+rl.on("line", async (c) => {
+  // This event fires every time the user presses Enter after typing something.
   if (c === "exit") return rl.close();
+  if (c.includes("echo")) {
+    const a = c.slice(5);
+    console.log(a)
+  }else{
   console.log(`${c}: command not found`);
+    }
   rl.prompt();
 });
